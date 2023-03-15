2d2cf9e14d08cb02128fbc33e4ce8bdfe4489d4e
athenaweb629 <athenaweb629@gmail.com>
2023-03-15 20:46:34 +0900

    update backend

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,8 +1,23 @@
 const db = require("../models");
 const User = db.user;
 exports.allAccess = (req, res) => {
-  console.log(req.user);
-  res.status(200).send("Public Content.");
+  User.find()
+    .populate("roles")
+    .then((users) => {
+      async function getUser() {
+        const filteredUsers = await users.filter((user) => {
+          return (
+            user.roles.filter((role) => role.name.toString() === "admin")
+              .length === 0
+          );
+        });
+        res.status(200).json({
+          message: "Fetched users successfully!",
+          data: filteredUsers,
+        });
+      }
+      getUser();
+    });
 };
 
 exports.userBoard = (req, res) => {
@@ -17,4 +32,44 @@ exports.adminBoard = (req, res) => {
 
 exports.moderatorBoard = (req, res) => {
   res.status(200).send("Moderator Content.");
+};
+
+exports.approveUser = (req, res) => {
+  User.findById(req.body.id).then((user) => {
+    if (!user) {
+      return res.status(404).json({
+        message: "Can not find User.",
+      });
+    } else {
+      const approve = req.body.approveStatus ? req.body.approveStatus : false;
+      user.approved = approve;
+      user
+        .save()
+        .then((user) => {
+          res.json({
+            message: "User has been changed successfully!",
+            data: user,
+          });
+        })
+        .catch((err) => {
+          res.json({
+            message: err,
+          });
+        });
+    }
+  });
+};
+
+exports.deleteUser = (req, res) => {
+  User.findOneAndDelete({ _id: req.params.id }).then((user) => {
+    if (!user) {
+      return res.status(404).json({
+        message: "Can't find User",
+      });
+    }
+    res.json({
+      message: "Deleted user successfully!",
+      data: user,
+    });
+  });
 };
